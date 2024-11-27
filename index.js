9f73acb684a2e8af5eab24773e4867132818f686
MELODY SIMWA <simwamelody@gmail.com>
2024-11-27 18:01:32 +0300

    create inex.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+var num1=31;
+var num2=2;
+const multiply =num1*num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 10) + 10; // Generates a random integer between 1 and 10
+
+const num3=46;
+const num4=7;
+const mod =num3 % num4;
+
+const max=(5,10,15,12,20);
